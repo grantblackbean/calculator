43634b57d06c7c8f330bd8f65385e1a8e3fffab2
grantblackbean <grant@blackbeancreative.com>
2020-02-27 13:54:06 -0800

    button add

---
+++ src/components/atoms/calcbutton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+const CalcButton = ({ onClick, title }) => (
+    <div className="my-2 px-2 w-1/3 overflow-hidden sm:my-2 sm:px-2 sm:w-1/3 md:my-2 md:px-2 md:w-1/3 lg:my-2 lg:px-2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={onClick}>{title}</button>
+    </div>
+)
+export default CalcButton;
